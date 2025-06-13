4db89d73de3abcf5ab94ee995915303e4778489c
charixe <charisa@gmail.com>
2025-06-13 11:58:21 +0700

    update file helloworld and variables

---
+++ src/com/project/java_basic/TypeVariables.java
@@ -0,0 +1,20 @@
+package com.project.java_basic;
+
+public class TypeVariables {
+    public static void main(String[] args) {
+        String name = "paerw"; //ประกาศตัวแปรแบบข้อความ
+        System.out.println(name);
+
+        int number = 7;
+        System.out.println(number);
+
+        double num = 15.44;
+        char A = 'b';
+        boolean result = false;
+        //Ture แปลว่า จริง
+        // False แปลว่า เท็จ
+
+        System.out.println(A+num+"paerw");
+
+    }
+}
